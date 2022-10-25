8ef28e7414084c7e1b34e727f4e1e84a4c957234
benwang2 <eb.benwang@gmail.com>
2022-10-25 00:16:38 -0400

    Uploaded rESTful API examples with flask-restx

--- week3/examples/restful_api.py
+++ week3/examples/restful_api.py
@@ -1,62 +1,50 @@
-from flask import Flask
-from flask_restful import reqparse, abort, Api, Resource
+from flask import Flask, send_file
+from flask_restx import reqparse, abort, Api, Resource
 
 app = Flask(__name__)
-api = Api(app)
+api = Api(doc=False)
+api.init_app(app)
 
-TODOS = {
-    'todo1': {'task': 'build an API'},
-    'todo2': {'task': '?????'},
-    'todo3': {'task': 'profit!'},
+@api.route("/index")
+class Index(Resource):
+   def get(self):
+    return send_file("index2.html")
+
+data = {
+    "fruits":   ["apple","cherry", "grape", "watermelon"],
+    "cars":     ["ford f150", "toyota RAV4", "honda accord"],
+    "jobs":     ["professor", "software engineer", "accountant"],
 }
 
-
-def abort_if_todo_doesnt_exist(todo_id):
-    if todo_id not in TODOS:
-        abort(404, message="Todo {} doesn't exist".format(todo_id))
-
 parser = reqparse.RequestParser()
-parser.add_argument('task')
+parser.add_argument("key")
+parser.add_argument("value")
 
-
-# Todo
-# shows a single todo item and lets you delete a todo item
-class Todo(Resource):
-    def get(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        return TODOS[todo_id]
-
-    def delete(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        del TODOS[todo_id]
-        return '', 204
-
-    def put(self, todo_id):
-        args = parser.parse_args()
-        task = {'task': args['task']}
-        TODOS[todo_id] = task
-        return task, 201
-
-
-# TodoList
-# shows a list of all todos, and lets you POST to add new tasks
-class TodoList(Resource):
+class Group(Resource):
     def get(self):
-        return TODOS
+        args = parser.parse_args()
+        group_name = args['key']
+        if not group_name:
+            return data
+        else:
+            if group_name in data:
+                return data[group_name], 200
+            else:
+                return {"message":"Could not find group for specified key","status":400}
 
     def post(self):
         args = parser.parse_args()
-        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
-        todo_id = 'todo%i' % todo_id
-        TODOS[todo_id] = {'task': args['task']}
-        return TODOS[todo_id], 201
+        key, value = args['key'], args['value']
+        if key in data:
+            if not value in data[key]:
+                data[key].append(value)
+                return {"message":"Resource was successfully added to group","statusCode":200}
+            else:
+                return {"message":"This resource already exists within the specified group.", "status":400}
+        else:
+            return {"message":"Could not find group for specified key","status":400}
 
-##
-## Actually setup the Api resource routing here
-##
-api.add_resource(TodoList, '/todos')
-api.add_resource(Todo, '/todos/<todo_id>')
-
+api.add_resource(Group, '/groups')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",debug=True)
